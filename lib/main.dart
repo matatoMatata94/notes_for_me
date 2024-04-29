7e1de0752a09ee9ac7a05bb87453b6fe400ab91b
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2024-04-29 20:01:43 +0200

    Updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_for_me/services/locas_storage_service.dart';
 import 'package:notes_for_me/view/note_list_view.dart';
+import 'package:notes_for_me/viewmodel/note_viewmodel.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -8,15 +9,31 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SharedPreferences prefs = await SharedPreferences.getInstance();
   LocalStorageService localStorageService = LocalStorageService(prefs);
-  runApp(MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        FutureProvider<LocalStorageService>.value(
+          value: Future.value(localStorageService),
+        ),
+        ChangeNotifierProvider<NoteViewModel>(
+          create: (_) => NoteViewModel(localStorageService),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  final LocalStorageService localStorageService;
+
+  MyApp({required this.localStorageService});
+
   @override
   Widget build(BuildContext context) {
-    return FutureProvider(
-      create: (_) => ,
-      initialData: null,
+    return FutureProvider<LocalStorageService>(
+      create: (_) => Future.value(localStorageService),
+      initialData: localStorageService,
       child: MaterialApp(
         title: 'Notes App',
         theme: ThemeData(
